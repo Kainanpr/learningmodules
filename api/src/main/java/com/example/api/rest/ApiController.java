65d18ba7acd8d401500b004311376ed5a16644d1
Kainan Ramos <kainanpr95@gmail.com>
2021-03-04 22:27:23 -0300

    Setup

---
+++ api/src/main/java/com/example/api/rest/ApiController.java
@@ -0,0 +1,16 @@
+package com.example.api.rest;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping
+    public ResponseEntity<String> testApi() {
+        return ResponseEntity.ok("Api OK");
+    }
+}
