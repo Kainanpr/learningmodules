b13b93e128074ff4523abb81997d490cc3d6a47b
Kainan Ramos <kainanpr95@gmail.com>
2021-03-09 23:06:48 -0300

    Fixed the pom in person module

--- person/src/main/kotlin/com/example/person/PersonApplication.kt
+++ person/src/main/kotlin/com/example/person/PersonApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KotlinApplication
+class PersonApplication
 
 fun main(args: Array<String>) {
-    runApplication<KotlinApplication>(*args)
+    runApplication<PersonApplication>(*args)
 }
